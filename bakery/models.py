369683e767ba4c00d11732f8db5613f57039cc2d
Olga <hijazin.olga@gmail.com>
2020-04-14 18:12:57 -0800

    Fixing Images

--- bakery/models.py
+++ bakery/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
 
 class Image(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='images', default=1)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True,upload_to='products')
     def __str__(self):
         return self.product.name
 
